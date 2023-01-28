aea10d818c73bf5c1d208c1837b3da8c61e4ee5a
jose66stack <jfp127631@gmail.com>
2023-01-27 21:15:12 -0600

    var

--- java.js
+++ java.js
@@ -1,1 +1,15 @@
-console.log("What is your favourite movie of all time?");
+const cars = ["BMW", "Mercedes", "Toyota"];
+cars[0] = "Honda";
+cars.push("Mitsubishi");
+
+document.getElementById("demo1").innerHTML = cars;
+
+var year = 2; // global variable
+let car = 20; // block variable
+let total = year * car;
+
+document.getElementById("demo").innerHTML = total;
+
+var name = "Im here"; // string variable
+
+console.log(name);
