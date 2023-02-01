af06ddc310a0945f32a240a51b7430b8b0845466
jose66stack <jfp127631@gmail.com>
2023-01-31 19:57:03 -0600

    variables
    second

--- java.js
+++ java.js
@@ -1,15 +1,21 @@
-const cars = ["BMW", "Mercedes", "Toyota"];
-cars[0] = "Honda";
-cars.push("Mitsubishi");
-
-document.getElementById("demo1").innerHTML = cars;
-
 var year = 2; // global variable
-let car = 20; // block variable
+{
+  var car = 20;
+  // block variable
+}
 let total = year * car;
 
 document.getElementById("demo").innerHTML = total;
 
-var name = "Im here"; // string variable
-
-console.log(name);
+("use strict");
+const x = Object.freeze({
+  name: "test",
+  age: 60,
+  obj: {
+    a: 1,
+  },
+});
+var health = "life"; // string variable
+console.log(health);
+console.log(x);
+x.obj.a = 5;
